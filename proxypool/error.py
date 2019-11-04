13b8a3a35857ff11cf1c9c5437b7c13f1ff82660
mingyang.liang <18790334713@163.com>
2019-11-04 22:46:17 +0800

    add db

---
+++ proxypool/error.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class PoolEmptyError(Exception):
+
+    def __init__(self):
+        Exception.__init__(self)
+
+    def __str__(self):
+        return repr('代理池已经枯竭')
