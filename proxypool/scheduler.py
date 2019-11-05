f78eceaa9c626bece80d6e09bc785ab7ec7e6c3b
mingyang.liang <18790334713@163.com>
2019-11-05 20:51:10 +0800

    添加调度模块

---
+++ proxypool/scheduler.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+TESTER_CYCLE = 20
+GETTER_CYCLE = 20
+TESTER_ENABLED = True
+GETTER_ENABLED = True
+API_ENABLED = True
+API_HOST = '0.0.0.0'
+API_PORT = '5555'
+
+import time
+from multiprocessing import Process
+from proxypool.api import app
+from proxypool.getter import Getter
+from proxypool.tester import Tester
+
+
+class Scheduler():
+    def schedule_tester(self, cycle=TESTER_CYCLE):
+        """
+        定时测试代理
+        """
+        tester = Tester()
+        while True:
+            print('测试器开始运行')
+            tester.run()
+            time.sleep(cycle)
+
+    def schedule_getter(self, cycle=GETTER_CYCLE):
+        """
+        定时获取代理
+        """
+        getter = Getter()
+        while True:
+            print('开始抓取代理')
+            getter.run()
+            time.sleep(cycle)
+
+    def schedule_api(self):
+        """
+        开启API
+        """
+        app.run(API_HOST, API_PORT)
+
+    def run(self):
+        print('代理池开始运行')
+        if TESTER_ENABLED:
+            tester_process = Process(target=self.schedule_tester)
+            tester_process.start()
+
+        if GETTER_ENABLED:
+            getter_process = Process(target=self.schedule_getter)
+            getter_process.start()
+
+        if API_ENABLED:
+            api_process = Process(target=self.schedule_api)
+            api_process.start()
