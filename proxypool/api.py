37d91300f46c66e154caf6c327c51eeb35d29636
mingyang.liang <18790334713@163.com>
2019-11-05 20:41:55 +0800

    add web api

--- proxypool/api.py
+++ proxypool/api.py
@@ -1,2 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from flask import Flask, g
+from proxypool.db import RedisClient
+
+app = Flask(__name__)
+
+
+def get_conn():
+    if not hasattr(g, 'redis'):
+        g.redis = RedisClient()
+    return g.redis
+
+
+@app.route('/')
+def index():
+    return '<h2>Welcome to Proxy Pool System</h2>'
+
+
+@app.route('/random')
+def get_proxy():
+    """
+    获取随机可用代理
+    :return: 随机代理
+    """
+    conn = get_conn()
+    return conn.random()
+
+
+@app.route('/count')
+def get_counts():
+    """
+    获取代理池总量
+    :return: 代理池总量
+    """
+    conn = get_conn()
+    return str(conn.count())
+
+
+if __name__ == '__main__':
+    app.run()
